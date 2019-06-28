c30ba601fbbee81a05836489546a18e0be126e03
Lucas Mansur <lucas.mansur2@gmail.com>
2019-06-28 15:43:21 -0300

    Initial commit

---
+++ lib/nexaas/queue_time.rb
@@ -0,0 +1,8 @@
+require "nexaas/queue_time/version"
+
+module Nexaas
+  module QueueTime
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/nexaas/queue_time/version.rb
@@ -0,0 +1,5 @@
+module Nexaas
+  module QueueTime
+    VERSION = "0.1.0"
+  end
+end
