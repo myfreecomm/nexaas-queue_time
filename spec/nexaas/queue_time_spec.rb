3a616e3058e933681b3e3cd9e88bc3bf54d01265
Lucas Mansur <lucas.mansur2@gmail.com>
2019-06-28 17:09:59 -0300

    Remove useless spec

--- spec/nexaas/queue_time_spec.rb
+++ spec/nexaas/queue_time_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Nexaas::QueueTime do
   it "has a version number" do
     expect(Nexaas::QueueTime::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
