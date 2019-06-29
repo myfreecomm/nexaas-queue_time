9f02bf40ef73bd8f3f3d6b801fda3bff6773b61f
Lucas Mansur <lucas.mansur2@gmail.com>
2019-06-29 11:47:07 -0300

    Test header for expected pattern before using it

--- lib/nexaas/queue_time/middleware.rb
+++ lib/nexaas/queue_time/middleware.rb
@@ -16,6 +16,14 @@ module Nexaas
     # This format is supported by APMs such as New Relic and Scout
     class Middleware
       METRIC_NAME = 'request.queue_time'
+      HEADER_FORMAT_PATTERN = /
+        ^   # Beginning of line
+        t=  #
+        \d+ # At least 1 digit
+        \.? # Optionally a dot may be used for fractional timestamps
+        \d* # Optionally more digits after the dot
+        $   # End of line
+      /x
 
       def initialize(app)
         @app = app
@@ -23,7 +31,7 @@ module Nexaas
 
       def call(env)
         request_start_header = env['HTTP_X_REQUEST_START']
-        if request_start_header
+        if request_start_header&.match?(HEADER_FORMAT_PATTERN)
           left_queue_at = Time.now.to_f
           metric = calculate_queue_time_in_ms(left_queue_at, request_start_header)
           send_metric(metric)
@@ -39,8 +47,6 @@ module Nexaas
         (left_queue_at - entered_queue_at.to_f) * 1000
       end
 
-      # The header actually comes as `t=1234567890`,
-      # so we need to extract the timestamp.
       def extract_timestamp(entered_queue)
         entered_queue.delete('t=')
       end
--- spec/nexaas/queue_time/middleware_spec.rb
+++ spec/nexaas/queue_time/middleware_spec.rb
@@ -12,7 +12,10 @@ RSpec.describe Nexaas::QueueTime::Middleware do
     let(:queue_time) { 0.5 }
     let(:request_start) { Time.now.to_f - queue_time }
     let(:request_env) do
-      { 'PATH_INFO' => '/index', 'HTTP_X_REQUEST_START' => "t=#{request_start}" }
+      {
+        'PATH_INFO' => '/index',
+        'HTTP_X_REQUEST_START' => "t=#{request_start}"
+      }
     end
 
     it 'does not alter request' do
@@ -42,6 +45,21 @@ RSpec.describe Nexaas::QueueTime::Middleware do
       subject.call(request_env)
     end
 
+    context 'when timestamp in HTTP_X_REQUEST_START is given as an integer' do
+      let(:queue_time) { 1 }
+      let(:request_start) { Time.now.to_i - queue_time }
+
+      it 'sends metric to statsd' do
+        queue_time_in_ms = queue_time * 1000
+        expect_any_instance_of(Datadog::Statsd).to receive(:timing).with(
+          described_class::METRIC_NAME,
+          be_within(2000).of(queue_time_in_ms),
+          sample_rate: 1
+        )
+        subject.call(request_env)
+      end
+    end
+
     context 'when HTTP_X_REQUEST_START is given as string' do
       let(:request_start) { (Time.now.to_f - queue_time).to_s }
 
@@ -52,6 +70,26 @@ RSpec.describe Nexaas::QueueTime::Middleware do
           be_within(0.3).of(queue_time_in_ms),
           sample_rate: 1
         )
+        subject.call(request_env)
+      end
+    end
+
+    context 'when HTTP_X_REQUEST_START is given in an unexpected format' do
+      before :each do
+        request_env['HTTP_X_REQUEST_START'] = 'something else'
+      end
+
+      it 'does not alter request' do
+        http_code, env, body = subject.call(request_env)
+
+        expect(http_code).to eq(200)
+        expect(env).to eq(request_env)
+        expect(body).to eq(['OK'])
+      end
+
+      it 'does not send metric to statsd' do
+        expect_any_instance_of(Datadog::Statsd).not_to receive(:timing)
+
         subject.call(request_env)
       end
     end
