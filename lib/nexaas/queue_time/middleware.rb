cfe1222f8454d39dcd8df9f723fe5867fafe3b42
Lucas Mansur <lucas.mansur2@gmail.com>
2019-08-21 19:08:54 -0300

    Wrap Datadog Statsd

--- lib/nexaas/queue_time/middleware.rb
+++ lib/nexaas/queue_time/middleware.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'datadog/statsd'
+require 'nexaas/queue_time/dogstatsd'
 
 module Nexaas
   module QueueTime
@@ -34,7 +34,7 @@ module Nexaas
         if request_start_header && request_start_header =~ HEADER_FORMAT_PATTERN
           left_queue_at = Time.now.to_f
           metric = calculate_queue_time_in_ms(left_queue_at, request_start_header)
-          send_metric(metric)
+          DogStatsd.timing(METRIC_NAME, metric.to_i, sample_rate: 1)
         end
 
         @app.call(env)
@@ -49,20 +49,6 @@ module Nexaas
 
       def extract_timestamp(entered_queue)
         entered_queue.delete('t=')
-      end
-
-      # By default, Datadog::Statsd opens a UDP connection with a given host and port.
-      # Instead, we are giving it a socket path so it communicates with the statsd server via UDS.
-      #
-      # This approach is easier to setup in containerized environments since all it requires
-      # is the path to the socket file instead of the host address.
-      #
-      # UDS also performs better than UDP,
-      # although the app would need to receive huge traffic to actually feel the difference.
-      def send_metric(metric)
-        Datadog::Statsd.open(nil, nil, socket_path: '/var/run/datadog/dsd.socket') do |statsd|
-          statsd.timing(METRIC_NAME, metric.to_i, sample_rate: 1)
-        end
       end
     end
   end
